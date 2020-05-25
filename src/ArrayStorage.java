b3c60e5c401351924a7daef0d45f9a464792ed09
Andrey Sablin <void.st@gmail.com>
2020-05-25 20:11:52 +0400

    working with NPE in GET

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,55 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int i = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        i=0;
     }
 
     void save(Resume r) {
+        storage[i] = r;
+        i++;
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.getUuid().equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
+
     }
 
     void delete(String uuid) {
+        int f=0;
+        for (int i1 = 0; i1 < i; i1++) {
+            if (storage[i1].getUuid().equals(uuid)){
+                 f=i1;
+            }
+        }
+        if (i - f >= 0) System.arraycopy(storage, f + 1, storage, f, i - f);
+        i--;
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result=new Resume[i];
+        System.arraycopy(storage, 0, result, 0, i);
+        return result;
     }
 
     int size() {
-        return 0;
+
+        return i;
     }
 }
